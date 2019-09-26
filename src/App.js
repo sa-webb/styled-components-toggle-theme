cb06c231794cd34be6daa47eef51be26d84eb64d
Austin Webb <stevenaustinwebb@gmail.com>
2019-09-26 11:50:58 -0500

    cleaned

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme } from './theme';
+import { GlobalStyles } from './global';
+import { useDarkMode } from './useDarkMode';
 
 function App() {
+  const [theme, toggleTheme, componentMounted] = useDarkMode();
+  const themeMode = theme === 'light' ? lightTheme : darkTheme;
+
+  if (!componentMounted) {
+    return <div />
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={themeMode}>
+      <>
+        <GlobalStyles />
+        <button onClick={toggleTheme}>Toggle theme</button>
+        <h1> It's a {theme === 'light' ? 'light theme' : 'dark theme'}</h1>
+        <footer></footer>
+      </>
+    </ThemeProvider>
   );
 }
 
