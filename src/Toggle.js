66af3a096ad02a701b4bf9ad161e5f210ef4f3e0
sa-webb <sawebb30@gmail.com>
2020-02-24 14:14:02 -0600

    ignore

--- src/Toggle.js
+++ src/Toggle.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { func, string } from 'prop-types';
-// Import a couple of SVG files we'll use in the design: https://www.flaticon.com
 import { ReactComponent as MoonIcon } from 'icons/moon.svg';
 import { ReactComponent as SunIcon } from 'icons/sun.svg';
 
